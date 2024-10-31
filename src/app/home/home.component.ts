22dc4e4a649fc5b96f874aab4bf5985e79020b45
Viral Patel <pviral3011@gmail.com>
2024-10-31 14:19:01 +0530

    implemented search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Food } from '../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
+import { SearchComponent } from "../search/search.component";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -14,14 +17,17 @@ export class HomeComponent implements OnInit {
   // private foodService = inject(FoodService);
 
   foods: Food[] = []
-  constructor(private foodService: FoodService) {
+  constructor(private foodService: FoodService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll();
-    console.log(this.foods)
+    this.route.params.subscribe(params => {
+      if (params['searchTerm']) {
+        this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase()
+          .includes(params['searchTerm'].toLowerCase()))
+      }
+      else this.foods = this.foodService.getAll();
+    })
   }
-
-
 }
